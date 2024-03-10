cacaf188ac2da59fcff486c13cc227f016aa075a
sajjad <sreyhani@ce.sharif.edu>
2024-03-10 12:38:29 +0330

    extract amount_for

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -12,43 +12,49 @@ pub fn statement(invoice: Value, plays: Value) -> String {
     let mut total_amount = 0;
     let mut volume_credits = 0;
     let mut result = format!("Statement for {}\n", invoice["customer"].as_str().unwrap());
+    let play_for = |perf: &Value| -> &Value { &plays[perf["playID"].as_str().unwrap()] };
 
-    for perf in invoice["performances"].as_array().unwrap() {
-        let play = &plays[perf["playID"].as_str().unwrap()];
-        let mut this_amount;
+    let amount_for = |perf: &Value, play: &Value| -> u64 {
+        let mut result;
         match play["type"].as_str().unwrap() {
             "tragedy" => {
-                this_amount = 40000;
+                result = 40000;
                 if perf["audience"].as_u64().unwrap() > 30 {
-                    this_amount += 1000 * (perf["audience"].as_u64().unwrap() - 30);
+                    result += 1000 * (perf["audience"].as_u64().unwrap() - 30);
                 }
             }
             "comedy" => {
-                this_amount = 30000;
+                result = 30000;
                 if perf["audience"].as_u64().unwrap() > 20 {
-                    this_amount += 10000 + 500 * (perf["audience"].as_u64().unwrap() - 20);
+                    result += 10000 + 500 * (perf["audience"].as_u64().unwrap() - 20);
                 }
-                this_amount += 300 * perf["audience"].as_u64().unwrap();
+                result += 300 * perf["audience"].as_u64().unwrap();
             }
             play_type => {
                 panic!("unknown type:{}", play_type);
             }
         }
+        result
+    };
+
+    for perf in invoice["performances"].as_array().unwrap() {
+
         // add volume credits
         volume_credits += max(perf["audience"].as_u64().unwrap() - 30, 0);
         // add extra credit for every ten comedy attendees
-        if "comedy" == play["type"].as_str().unwrap() {
+        if "comedy" == play_for(perf)["type"].as_str().unwrap() {
             volume_credits += (perf["audience"].as_f64().unwrap() / 5.0).floor() as u64;
         }
         // print line for this order
         result += &format!(
             " {}: {} ({} seats)\n",
-            play["name"].as_str().unwrap(),
-            usd(this_amount as f64 / 100 as f64).format(),
+            play_for(perf)["name"].as_str().unwrap(),
+            usd(amount_for(perf, play_for(perf)) as f64 / 100 as f64).format(),
             perf["audience"].as_u64().unwrap()
         );
-        total_amount += this_amount;
+        total_amount += amount_for(perf, play_for(perf));
     }
+
     result += &format!(
         "Amount owed is {}\n",
         usd(total_amount as f64 / 100 as f64).format()

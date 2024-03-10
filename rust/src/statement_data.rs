5fdfe148529a9615e18e33501090d947bdfd9931
sajjad <sreyhani@ce.sharif.edu>
2024-03-10 17:40:07 +0330

    impl calculators

--- rust/src/statement_data.rs
+++ rust/src/statement_data.rs
@@ -4,25 +4,44 @@ use crate::types::{Invoice, Performance, Play, Plays};
 
 trait PerformanceCalculator {
     fn amount(&self) -> u64;
+    fn volume_credit(&self) -> u64;
 }
 
 struct ComedyPerformanceCalculator<'a> {
     perf: &'a Performance,
-    play: &'a Play,
 }
+
 impl<'a> PerformanceCalculator for ComedyPerformanceCalculator<'a> {
     fn amount(&self) -> u64 {
-        0
+        let mut result = 30000;
+        if self.perf.audience > 20 {
+            result += 10000 + 500 * (self.perf.audience - 20);
+        }
+        result += 300 * self.perf.audience;
+        result
+    }
+
+    fn volume_credit(&self) -> u64 {
+        let mut result = max(self.perf.audience - 30, 0);
+        result += (self.perf.audience as f64 / 5.0).floor() as u64;
+        result
     }
 }
 
 struct TragedyPerformanceCalculator<'a> {
     perf: &'a Performance,
-    play: &'a Play,
 }
 impl<'a> PerformanceCalculator for TragedyPerformanceCalculator<'a> {
     fn amount(&self) -> u64 {
-        0
+        let mut result = 40000;
+        if self.perf.audience > 30 {
+            result += 1000 * (self.perf.audience - 30);
+        }
+        result
+    }
+
+    fn volume_credit(&self) -> u64 {
+        max(self.perf.audience - 30, 0)
     }
 }
 
@@ -33,60 +52,40 @@ pub struct StatementData<'a> {
 
 pub struct EnrichedPerformance<'a> {
     calculator: Box<dyn PerformanceCalculator + 'a>,
-    perf:  &'a Performance,
-    plays: &'a Plays,
+    perf: &'a Performance,
+    play: &'a Play,
 }
 
 impl<'a> EnrichedPerformance<'a> {
     pub fn new(perf: &'a Performance, plays: &'a Plays) -> Self {
-        let calculator = Self::create_performance_calculator(perf, &plays[&perf.play_id]);
+        let play = &plays[&perf.play_id];
+        let calculator = Self::create_performance_calculator(perf, play);
         EnrichedPerformance {
             perf,
-            plays,
+            play,
             calculator,
         }
     }
-    fn create_performance_calculator(perf: &'a Performance, play: &'a Play) -> Box<dyn PerformanceCalculator + 'a> {
+    fn create_performance_calculator(
+        perf: &'a Performance,
+        play: &'a Play,
+    ) -> Box<dyn PerformanceCalculator + 'a> {
         match play.p_type.as_str() {
-            "tragedy" => Box::new(TragedyPerformanceCalculator { perf, play }),
-            "comedy" => Box::new(ComedyPerformanceCalculator { perf, play }),
+            "tragedy" => Box::new(TragedyPerformanceCalculator { perf }),
+            "comedy" => Box::new(ComedyPerformanceCalculator { perf }),
             play_type => panic!("unknown type:{}", play_type),
         }
     }
     pub fn amount(&self) -> u64 {
-        let mut result;
-        match self.play().p_type.as_str() {
-            "tragedy" => {
-                result = 40000;
-                if self.perf.audience > 30 {
-                    result += 1000 * (self.perf.audience - 30);
-                }
-            }
-            "comedy" => {
-                result = 30000;
-                if self.perf.audience > 20 {
-                    result += 10000 + 500 * (self.perf.audience - 20);
-                }
-                result += 300 * self.perf.audience;
-            }
-            play_type => {
-                panic!("unknown type:{}", play_type);
-            }
-        }
-        result
+        self.calculator.amount()
     }
 
     pub fn volume_credits(&self) -> u64 {
-        let mut result = max(self.perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if "comedy" == self.play().p_type {
-            result += (self.perf.audience as f64 / 5.0).floor() as u64;
-        }
-        result
+        self.calculator.volume_credit()
     }
 
     pub fn play(&self) -> &Play {
-        &self.plays[&self.perf.play_id]
+        self.play
     }
 
     pub fn audience(&self) -> u64 {

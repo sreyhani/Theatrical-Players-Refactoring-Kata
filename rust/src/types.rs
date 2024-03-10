fa73939e3ede009350722b468708b6309520e041
sajjad <sreyhani@ce.sharif.edu>
2024-03-10 17:46:13 +0330

    remove clonable

--- rust/src/types.rs
+++ rust/src/types.rs
@@ -2,20 +2,20 @@ use std::collections::HashMap;
 
 use serde::Deserialize;
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize)]
 pub struct Performance {
     #[serde(alias = "playID")]
     pub(crate) play_id: String,
     pub(crate) audience: u64,
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize)]
 pub struct Invoice {
     pub(crate) customer: String,
     pub(crate) performances: Vec<Performance>,
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize)]
 pub struct Play {
     pub(crate) name: String,
     #[serde(alias = "type")]
